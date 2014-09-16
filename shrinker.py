b97cdbf43ab0e386aa9ed8c07be29198b7fff2ec
Jeroen Doggen <jeroendoggen@gmail.com>
2014-09-16 20:19:17 +0200

    Moved all stubs to a class

--- shrinker.py
+++ shrinker.py
@@ -13,41 +13,50 @@ INPUTFOLDER = SCRIPTPATH + "/input_original"
 OUTPUTFOLDER = SCRIPTPATH + "/output_lowres"
 
 
-def run():
-    """Run the main program"""
-    create_folders()
-    for (everyfile)
-        get_type()
-        define_work()
-        convert()
-        report()
+class MultimediaShrinker:
+    """ Contains all the code for the tool """
 
-def report():
-    """Calculate the progress, file size, ..."""
+    def __init__(self):
+        self.settings =
 
-def create_folders():
-    """Duplicate the input folder structure in the output folder"""
+    def run(self):
+        """Run the main program"""
+        create_folders()
+        for (everyfile)
+            get_type()
+            define_work()
+            convert()
+            report()
 
-def get_type(filename):
-    """Detect the file type (image/video,...)"""
-    pass
+    def report():
+        """Calculate the progress, file size, ..."""
+        pass
 
-def convert(filename, filetype):
-    """Convert the file to a 'lowres' version"""
-    pass
+    def create_folders():
+        """Duplicate the input folder structure in the output folder"""
+        pass
 
-def define_work(filename)
-    """Analyse the file and decide what to do"""
-    pass
+    def get_type(filename):
+        """Detect the file type (image/video,...)"""
+        pass
 
-def get_size(start_path='.'):
-    """ Calculate folder size """
-    total_size = 0
-    for dirpath, dirnames, filenames in os.walk(start_path):
-        for inputfile in filenames:
-            filepointer = os.path.join(dirpath, inputfile)
-            total_size += os.path.getsize(filepointer)
-    return float(total_size)
+    def convert(filename, filetype):
+        """Convert the file to a 'lowres' version"""
+        pass
+
+    def define_work(filename)
+        """Analyse the file and decide what to do"""
+        pass
+
+    def get_size(start_path='.'):
+        """ Calculate folder size """
+        total_size = 0
+        for dirpath, dirnames, filenames in os.walk(start_path):
+            for inputfile in filenames:
+                filepointer = os.path.join(dirpath, inputfile)
+                total_size += os.path.getsize(filepointer)
+        return float(total_size)
 
 if __name__ == "__main__":
-    sys.exit(run())
+    shrinker = MultimediaShrinker()
+    sys.exit(shrinker.run())
